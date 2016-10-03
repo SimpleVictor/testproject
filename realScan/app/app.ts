0c7a9a4bf011fee729198a07d011725589e0f8a2
VietAnhh <viet.le175@gmail.com>
2016-10-03 04:09:43 -0400

    sqlstorage / sqllite

--- realScan/app/app.ts
+++ realScan/app/app.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { Platform, ionicBootstrap } from 'ionic-angular';
-import { StatusBar } from 'ionic-native';
+import {Platform, ionicBootstrap, SqlStorage, Storage} from 'ionic-angular';
+import {StatusBar, SQLite} from 'ionic-native';
 import {LoginPage} from "./pages/login/login";
 import {FirebaseService} from "./provider/firebase";
 import {QrService} from "./provider/qrservice";
@@ -22,11 +22,35 @@ export class MyApp {
 
 
     platform.ready().then(() => {
-      console.log("went in here");
       console.log(platform.platforms());
-
-
-
+      let myPlat = platform.platforms();
+      if(myPlat[0] === 'cordova'){
+        console.log("You are deploying the app on an Iphone");
+        let db = new SQLite();
+        db.openDatabase({
+          name: 'data.db',
+          location: 'default'
+        }).then(() => {
+              db.executeSql("CREATE TABLE IF NOT EXISTS current_user (id INTEGER PRIMARY KEY AUTOINCREMENT, name text)", [])
+                .then((data) => {
+                  console.log("Table has been created : "+data);
+                }, (error) => {
+                  console.error("Unable to open database", error);
+                });
+        });
+      }else{
+        console.log("You are running your device on a Web Application");
+        let storage = new Storage(SqlStorage);
+        storage.query('CREATE TABLE IF NOT EXISTS current_user (id INTEGER PRIMARY KEY AUTOINCREMENT, food_id text, food_name text, is_saved text, is_recent text, is_scanned text)').then(
+          result => {
+            console.log(result);
+            console.log("Created Table Successfully");
+          }, err => {
+            console.log("Failed Making Table BOoo");
+            console.log(err);
+          }
+        );
+      }
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
--- realScan/app/provider/sqlstorage.ts
+++ realScan/app/provider/sqlstorage.ts
@@ -4,7 +4,6 @@ import { Injectable } from '@angular/core';
 export class SQLStorage{
 
     constructor() {
-      console.log("I hate everyone");
     }
 
 }
